c12d5c6f51c4cf97969b145bfaa6851e29086e57
chimbox <chimboxsoftgve@gmail.com>
2021-03-16 01:19:33 -0700

    Se agregó una entidad de propuesta

---
+++ src/java/entities/Alumno.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Alfonso Felix
+ */
+@Entity
+public class Alumno extends EntityBase implements Serializable {
+
+    private String nombre;
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
